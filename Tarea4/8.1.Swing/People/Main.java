494a9a5f9a162fca209a3c219a6d10d1e6141c6e
Josemolina993 <67528839+Josemolina993@users.noreply.github.com>
2025-01-20 17:12:35 -0500

    Delete Tarea4/8.1.Swing directory

--- Tarea4/8.1.Swing/People/Main.java
+++
@@ -1,9 +0,0 @@
-package People;
-
-public class Main {
-    
-    public static void main(String[] args) {
-        MainWindow mainWindow = new MainWindow();
-        mainWindow.setVisible(true);
-    }
-}
