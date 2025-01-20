32c9d603a4abcda3e64d2329100f8a7954cbdf06
Josemolina993 <67528839+Josemolina993@users.noreply.github.com>
2025-01-20 16:55:48 -0500

    Add files via upload

---
+++ Tarea4/8.1.Swing/People/Main.java
@@ -0,0 +1,9 @@
+package People;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        MainWindow mainWindow = new MainWindow();
+        mainWindow.setVisible(true);
+    }
+}
---
+++ Tarea4/8.1.Swing/People/Person.java
@@ -0,0 +1,16 @@
+package People;
+
+public class Person {
+    String name;
+    String lastName;
+    String phone;
+    String address;
+
+    public Person(String name, String lastName, String phone, String address) {
+        this.name = name;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.address = address;
+    }
+    
+}
